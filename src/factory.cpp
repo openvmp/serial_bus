ac1796f85345316c559cf77bd4780a071b4db813
Roman Kuzmenko <clairbee@guidemaze.com>
2023-03-18 11:55:46 -0700

    refactored the factory interface to enable control of remote behavior for multiple interfaces using the same parameter

--- src/factory.cpp
+++ src/factory.cpp
@@ -9,14 +9,20 @@
 
 #include "serial_bus/factory.hpp"
 
-#include "serial_bus/interface_remote.hpp"
 #include "serial_bus/implementation.hpp"
+#include "serial_bus/interface_remote.hpp"
 
 namespace serial_bus {
 
 std::shared_ptr<Interface> Factory::New(rclcpp::Node *node) {
+  rclcpp::Parameter use_remote;
+  if (!node->has_parameter("use_remote")) {
+    node->declare_parameter("use_remote", true);
+  }
+  node->get_parameter("use_remote", use_remote);
+
   rclcpp::Parameter is_remote;
-  node->declare_parameter("serial_bus_is_remote", true);
+  node->declare_parameter("serial_bus_is_remote", use_remote.as_bool());
   node->get_parameter("serial_bus_is_remote", is_remote);
 
   if (is_remote.as_bool()) {
